d4643a83ccfb89f1f422209076ca6124f27b52f1
Dhairya Surana <dhairs7@vt.edu>
2020-12-06 14:34:16 -0500

    Integrated code to display scraper results in same window

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QComboBox
 from PyQt5.QtCore import QFile
 
+from scraper import *
+
 
 class Window(QtWidgets.QMainWindow):
     def __init__(self):
@@ -13,11 +15,34 @@ class Window(QtWidgets.QMainWindow):
         uic.loadUi('form.ui', self) # Load the .ui file
         self.show() # Show the GUI
 
-        dropdown = self.findChild(QtWidgets.QComboBox, "games_dropdown")
-        dropdown.addItem("Witcher 3")
-        dropdown.addItem("Skyrim")
-        dropdown.addItem("Witcher 1")
-        dropdown.addItem("Witcher 2")
+        # dropdown = self.findChild(QtWidgets.QComboBox, "games_dropdown")
+        # dropdown.addItem("Witcher 3")
+        # dropdown.addItem("Skyrim")
+        # dropdown.addItem("Witcher 1")
+        # dropdown.addItem("Witcher 2")
+
+        self.searchbox = self.findChild(QtWidgets.QLineEdit, "searchbox")
+        self.searchbutton = self.findChild(QtWidgets.QPushButton, "searchbutton")
+        self.game_list = self.findChild(QtWidgets.QTextEdit, "game_list")
+        self.game_list.setReadOnly(True)
+
+        self.searchbutton.clicked.connect(self.searchButtonEvent)
+
+    def searchButtonEvent(self):
+        
+        text = self.searchbox.displayText()
+
+        if text:
+            print(text)
+            self.searchbox.clear()
+
+        books = getBooks(text)
+
+        for items in books:
+            for i in items:
+                self.game_list.append(i)
+
+
 
 
 if __name__ == "__main__":

51f13dc255b989cacd3f994e707b07f083a62904
Grant-Syt <grantsyt21@gmail.com>
2020-12-06 12:57:20 -0500

    implemented search bar

--- main.py
+++ main.py
@@ -13,11 +13,19 @@ class Window(QtWidgets.QMainWindow):
         uic.loadUi('form.ui', self) # Load the .ui file
         self.show() # Show the GUI
 
-        dropdown = self.findChild(QtWidgets.QComboBox, "games_dropdown")
-        dropdown.addItem("Witcher 3")
-        dropdown.addItem("Skyrim")
-        dropdown.addItem("Witcher 1")
-        dropdown.addItem("Witcher 2")
+        # dropdown = self.findChild(QtWidgets.QComboBox, "games_dropdown")
+        # dropdown.addItem("Witcher 3")
+        # dropdown.addItem("Skyrim")
+        # dropdown.addItem("Witcher 1")
+        # dropdown.addItem("Witcher 2")
+
+        self.searchbox = self.findChild(QtWidgets.QLineEdit, "searchbox")
+        self.searchbutton = self.findChild(QtWidgets.QPushButton, "searchbutton")
+        self.plaintextedit = self.findChild(QtWidgets.QPlainTextEdit, "plaintextedit")
+        self.searchbutton.clicked.connect(self.searchButtonEvent)
+    def searchButtonEvent(self):
+        print(self.searchbox.displayText())
+
 
 
 if __name__ == "__main__":
